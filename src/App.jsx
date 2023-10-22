d51f9def6ba800105efe661bf00926089c971699
Tigran Soghomonyan <soghomonyantigran@gmail.com>
2023-10-22 21:49:56 +0400

    [feature] Added CardButton. Used composition

--- src/App.jsx
+++ src/App.jsx
@@ -2,34 +2,44 @@ import Button from "./components/Button";
 import JournalItem from "./components/JournalItem";
 
 import "./App.css";
+import CardButton from "./components/CardButton/CardButton";
 
 function App() {
-  const data = [
-    {
-      title: "Title 1",
-      date: new Date(),
-      text: "Text 1"
-    },
-    {
-      title: "Title 2",
-      date: new Date(),
-      text: "Text 2"
-    },
-    {
-      title: "Title 3",
-      date: new Date(),
-      text: "Text 3"
-    }
-  ];
+	const data = [
+		{
+			title: "Title 1",
+			date: new Date(),
+			text: "Text 1"
+		},
+		{
+			title: "Title 2",
+			date: new Date(),
+			text: "Text 2"
+		},
+		{
+			title: "Title 3",
+			date: new Date(),
+			text: "Text 3"
+		}
+	];
 
-  return (
-    <>
-      <Button accent name="Save" />
-      <JournalItem title={data[0].title} date={data[0].date} text={data[0].text} />
-      <JournalItem title={data[1].title} date={data[1].date} text={data[1].text} />
-      <JournalItem title={data[2].title} date={data[2].date} text={data[2].text} />
-    </>
-  );
+	return (
+		<>
+			<Button accent name="Save" />
+			<CardButton>
+        New memory
+			</CardButton>
+			<CardButton>
+				<JournalItem title={data[0].title} date={data[0].date} text={data[0].text} />
+			</CardButton>
+			<CardButton>
+				<JournalItem title={data[1].title} date={data[1].date} text={data[1].text} />
+			</CardButton>
+			<CardButton>
+				<JournalItem title={data[2].title} date={data[2].date} text={data[2].text} />
+			</CardButton>
+		</>
+	);
 }
 
 export default App;
--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,6 +1,5 @@
 import "./Button.css";
 
-// eslint-disable-next-line react/prop-types
 export default function Button({ accent, name }) {
-    return <button className={`button ${accent ? "accent" : ""}`}>{name}</button>;
+	return <button className={`button ${accent ? "accent" : ""}`}>{name}</button>;
 }
