7b6b1af5e9ef7dc9b889c65149779dafbcfe0100
Tigran Soghomonyan <soghomonyantigran@gmail.com>
2023-10-29 00:57:20 +0400

    [feature] Added conditional styling (version 1)

--- src/components/JournalForm/JournalForm.jsx
+++ src/components/JournalForm/JournalForm.jsx
@@ -1,21 +1,52 @@
+import { useState } from "react";
+
 import Button from "../Button";
 
 import "./JournalForm.css";
 
 export default function JournalForm({ onSubmit }) {
+  const [formValidState, setFormValidState] = useState({
+    title: true,
+    post: true,
+    date: true,
+  })
   const addJournalItem = function (event) {
     event.preventDefault();
     const formData = new FormData(event.target)
     const formProps = Object.fromEntries(formData)
+
+    let isFormValid = true;
+    if (!formProps.title?.trim().length) {
+      setFormValidState(state => ({ ...state, title: false }));
+      isFormValid = false;
+    } else {
+      setFormValidState(state => ({ ...state, title: true }))
+    }
+
+    if (!formProps.post?.trim().length) {
+      setFormValidState(state => ({ ...state, post: false }));
+      isFormValid = false;
+    } else {
+      setFormValidState(state => ({ ...state, post: true }))
+    }
+
+    if (!formProps.date) {
+      setFormValidState(state => ({ ...state, date: false }));
+      isFormValid = false;
+    } else {
+      setFormValidState(state => ({ ...state, date: true }))
+    }
+    if (!isFormValid) return
+
     onSubmit(formProps)
   };
 
   return (
     <form className="journal-form" onSubmit={addJournalItem}>
-      <input type="text" name="title" />
-      <input type="date" name="date" />
+      <input type="text" name="title" style={{ border: formValidState.title ? undefined : '1px solid #d00' }} />
+      <input type="date" name="date" style={{ border: formValidState.date ? undefined : '1px solid #d00' }} />
       <input type="text" name="tag" />
-      <textarea name="text"></textarea>
+      <textarea name="post" style={{ border: formValidState.post ? undefined : '1px solid #d00' }}></textarea>
       <Button accent name="Save" />
     </form>
   );
